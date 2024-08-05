9588c2a8a1067b1870cd0c451d21b87791fb80e6
ISHITA <91796142+Ishita2407@users.noreply.github.com>
2024-08-05 17:45:28 +0530

    Create Second max in arr.java

---
+++ Second max in arr.java
@@ -0,0 +1,16 @@
+public class Solution{
+  public int secondMax(int arr[]){
+    int max = Integer.MIN_VALUE;
+    int secMax = Integer.MIN_VALUE;
+
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] > max){
+        secMax = max;
+        max = arr[i];
+      }else if(arr[i] > secMax && arr[i] != max){
+        secMax = arr[i];
+      }
+    }
+    return secMax;
+  }
+}
